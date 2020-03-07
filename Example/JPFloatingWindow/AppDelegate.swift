8170d751ac775ae33db5109a5ae9a7cdbf596249
ZhouJianPing <zhoujianping24@hotmail.com>
2020-03-07 21:58:32 +0800

    update

--- Example/JPFloatingWindow/AppDelegate.swift
+++ Example/JPFloatingWindow/AppDelegate.swift
@@ -15,7 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+//        var count: UInt32 = 0
+//        let methodList = class_copyMethodList(UINavigationController.self, &count)
+//        for i in 0 ..< count {
+//            let method = methodList![Int(i)]
+//            let name = sel_getName(method_getName(method))
+//            print(String(cString: name))
+//        }
+//        free(methodList)
+        
+        _ = JPFwManager
+        
         return true
     }
 
